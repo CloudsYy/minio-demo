0d99bf98d7977690268935feeee8f3bc9ef0de3e
Cloud <hongzezhi_email@126.com>
2020-03-16 17:08:47 +0800

    下载文件

--- src/main/java/com/example/miniodemo/Controller/MinioController.java
+++ src/main/java/com/example/miniodemo/Controller/MinioController.java
@@ -24,6 +24,7 @@ public class MinioController {
             InputStream is = file.getInputStream();//获取文件流
             String filename = file.getOriginalFilename();//获取文件名称
             String contentType = file.getContentType();//获取文件类型
+            long size = file.getSize();
             minioClient.putObject("files",filename,is,contentType);//添加文件到(minio)存储桶中
 //            long l1 = System.currentTimeMillis();
 //            System.out.println("耗时多少："+(l1-l));
@@ -41,10 +42,11 @@ public class MinioController {
     /*
     * 下载文件
     * presignedGetObject(String bucketName, String objectName, Integer expires)
-    * 生成一个给HTTP GET请求用的presigned URL。浏览器/移动端的客户端可以用这个URL进行下载，即使其所在的存储桶是私有的。这个presigned URL可以设置一个失效时间，默认值是7天。
+    * 生成一个给HTTP GET请求用的presigned URL。浏览器/移动端的客户端可以用这个URL进行下载，
+    * 即使其所在的存储桶是私有的。这个presigned URL可以设置一个失效时间，默认值是7天。
     * */
     @GetMapping("download")
-    public String downloadFiles(@RequestParam String filename){
+    public String downloadFiles(@RequestParam("filename") String filename){
         try {
             MinioClient minioClient = new MinioClient(url,accessKey,secretKey);
 //            List<Bucket> buckets = minioClient.listBuckets();
@@ -63,4 +65,5 @@ public class MinioController {
             return "获取失败";
         }
     }
+
 }
